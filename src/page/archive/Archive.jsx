b4e55ce15c0b2201819ef73491dbfc81228f42e7
Sahil Padvi <sahilpadvi25@gmail.com>
2022-04-11 23:21:48 +0530

    style fix

--- src/page/archive/Archive.jsx
+++ src/page/archive/Archive.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import { useArchivesList } from '../../context/context-index';
-import Lottie from 'react-lottie';
-import animationData from '../../assets/lottie-animation/empty-list';
 import Nodata from '../../assets/image/No data-cuate.svg';
-
 import './Archive.css';
 import { NoteCard } from '../../component/component-index';
 
 export const Archive = () => {
   const { archivesList } = useArchivesList();
-  const lottieOption = {
-    loop: false,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  };
 
   return (
     <div className="main-page">
@@ -26,7 +15,6 @@ export const Archive = () => {
             return <NoteCard key={item._id} item={item} archive={true} />;
           })
         ) : (
-          // <Lottie options={lottieOption} height={400} width={400} />
           <div className="empty-list-image">
             <img src={Nodata} alt="list image" />
           </div>
